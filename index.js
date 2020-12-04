3c3f065625c6942130557080e73cb151d56ca326
saoussen <sawsan_sli3i@live.fr>
2020-12-04 20:04:45 +0000

    modified my code pass the correct information into the constructors

--- index.js
+++ index.js
@@ -10,45 +10,48 @@ class Account {
 }
 
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
-  }
-
-}
 class Deposit {
 
-  constructor(amount) {
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
+  // Update the balance in the account
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 
 }
 
+class Withdrawal {
 
+  // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  // Update the balance in the account
+  commit() {
+    this.account.balance -= this.amount;
+  }
 
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
 console.log('Balance:', balance);
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
