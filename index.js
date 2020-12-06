e3ce7c8f2d4d3a640cfe00e5e008c9c0f9e47c5a
saoussen <sawsan_sli3i@live.fr>
2020-12-06 01:29:43 +0000

    added a list of transactions

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.Transactions = [];
+  }
+  get balance() {
+    let s = 0;
+    for (const t of this.Transactions) {
+      s += t.value;
+      console.log(s);
+    }
+    return (balance + s);
+  }
+
+  addTransaction(transaction) {
+    this.Transactions.push(transaction);
   }
 
 }
@@ -14,7 +26,7 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    balance += this.value;
   }
 
 }
@@ -45,12 +57,17 @@ const myAccount = new Account("snow-patrol");
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
-
+myAccount.addTransaction(t1);
 t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
-
+myAccount.addTransaction(t2);
 console.log('Balance:', balance);
 t3 = new Deposit(120.00, myAccount);
 t3.commit();
+myAccount.addTransaction(t3);
+
 console.log('Transaction 3:', t3);
+
+console.log(myAccount.Transactions);
+console.log(myAccount.balance);
